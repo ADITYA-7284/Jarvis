c429695feb045b5df15a59b9da96a1a2c7637c24
Aditya Thakur <adityakumar865189@gmail.com>
2026-07-28 18:53:45 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,16 @@
+music = {
+    "amplifier": "https://www.youtube.com/watch?v=uuCFRaFWjwY",
+    "illahi": "https://www.youtube.com/watch?v=fdubeMFwuGs",
+    "my mashup": "https://www.youtube.com/watch?v=Awly2qTcOGo&list=PPSV",
+    "raabta": "https://www.youtube.com/watch?v=zlt38OOqwDc", 
+    "kesariya": "https://www.youtube.com/watch?v=BddP6PYo2gs",
+    "baarishein": "https://www.youtube.com/watch?v=PJWemSzExXs",
+    "afreen afreen": "https://www.youtube.com/watch?v=kw4tT7SCmaY",
+    "hawayein": "https://www.youtube.com/watch?v=cs1e0fRyI18",
+    "shayad": "https://www.youtube.com/watch?v=MJyKN-8UncM",
+    "husn": "https://www.youtube.com/watch?v=gJLVTKhTnog",
+    "sahiba": "https://www.youtube.com/watch?v=n2dVFdqMYGA",
+    "mera yaar": "https://www.youtube.com/watch?v=ycS5PagXvhQ",
+    "chaudhary": "https://www.youtube.com/watch?v=1gukvtH_a3I",
+    "ranjheya ve": "https://www.youtube.com/watch?v=55c6IlV7BEo"
+}
